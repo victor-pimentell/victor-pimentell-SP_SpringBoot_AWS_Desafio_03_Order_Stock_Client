52da233c904b04419e9a88983e5b7a0a1d588e90
Victor Pimentel Correia <victorpimentelw@gmail.com>
2024-11-27 15:30:18 -0300

    :tada: orderms first commit

---
+++ orderms/src/main/java/com/github/victor/orderms/OrdermsApplication.java
@@ -0,0 +1,13 @@
+package com.github.victor.orderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermsApplication.class, args);
+	}
+
+}
