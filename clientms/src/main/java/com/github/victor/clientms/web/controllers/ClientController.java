9ce088ad36286828528060314ed2bb554980d8fc
Victor Pimentel Correia <victorpimentelw@gmail.com>
2024-11-30 17:54:20 -0300

    :sparkles: feat: hateoas implemented on clientms

--- clientms/src/main/java/com/github/victor/clientms/web/controllers/ClientController.java
+++ clientms/src/main/java/com/github/victor/clientms/web/controllers/ClientController.java
@@ -2,15 +2,13 @@ package com.github.victor.clientms.web.controllers;
 
 import com.github.victor.clientms.entities.Client;
 import com.github.victor.clientms.services.ClientService;
+import com.github.victor.clientms.util.HateoasUtil;
 import com.github.victor.clientms.web.dto.*;
 import com.github.victor.clientms.web.dto.mapper.ClientMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,29 +21,22 @@ public class ClientController {
     @GetMapping("/email/{email}")
     public ResponseEntity<ClientResponseDto> getClientByEmail(@PathVariable String email) {
         Client client = clientService.getClientByEmail(email);
-        ClientResponseDto responseDto = ClientMapper.toDto(client);
+        ClientResponseDto responseDto = HateoasUtil.hateoasClientByEmail(client);
         return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping("/id/{id}")
     public ResponseEntity<ClientResponseDto> getClientById(@PathVariable Long id) {
         Client client = clientService.getClientById(id);
-        ClientResponseDto responseDto = ClientMapper.toDto(client);
+        ClientResponseDto responseDto = HateoasUtil.hateoasClientById(client);
         return ResponseEntity.ok(responseDto);
     }
 
     @PostMapping
     public ResponseEntity<ClientResponseDto> createClient(@Valid @RequestBody ClientCreateDto clientCreateDto) {
-        Client client = clientService.createProduct(clientCreateDto);
-        ClientResponseDto responseDto = ClientMapper.toDto(client);
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(client.getId())
-                .toUri();
-
-        return ResponseEntity.created(location).body(responseDto);
+        Client client = clientService.createClient(clientCreateDto);
+        ClientResponseDto responseDto = HateoasUtil.hateoesCreateClient(client);
+        return ResponseEntity.ok(responseDto);
     }
 
     @PutMapping
--- clientms/src/main/java/com/github/victor/clientms/web/dto/ClientResponseDto.java
+++ clientms/src/main/java/com/github/victor/clientms/web/dto/ClientResponseDto.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ClientResponseDto {
+public class ClientResponseDto extends RepresentationModel<ClientResponseDto> implements Serializable {
     private Long id;
     private String name;
     private String email;
