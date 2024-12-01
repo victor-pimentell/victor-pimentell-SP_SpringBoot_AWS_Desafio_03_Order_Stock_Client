17f01e54c48221cda50cb11f30ebe2f6af32fceb
Victor Pimentel Correia <victorpimentelw@gmail.com>
2024-12-01 04:37:36 -0300

    :sparkles: feat: documentation swagger implemented on orderms

--- orderms/src/main/java/com/github/victor/orderms/web/controllers/OrderController.java
+++ orderms/src/main/java/com/github/victor/orderms/web/controllers/OrderController.java
@@ -18,7 +18,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
