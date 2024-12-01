74cec0d36cce771d61da1b50c3fab005942a0da5
Victor Pimentel Correia <victorpimentelw@gmail.com>
2024-12-01 04:36:48 -0300

    :sparkles: feat: documentation swagger implemented on clientms

--- clientms/src/main/java/com/github/victor/clientms/web/controllers/ClientController.java
+++ clientms/src/main/java/com/github/victor/clientms/web/controllers/ClientController.java
@@ -14,7 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clients")
+@RequestMapping("/api/v1/clients")
 @RequiredArgsConstructor
 public class ClientController {
 
